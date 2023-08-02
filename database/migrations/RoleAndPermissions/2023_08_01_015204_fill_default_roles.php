cda99d255a284b99374e944cd8e4c8ca31c5b45f
sabirov_rashit <rashit.sabirov1999@gmail.com>
2023-08-02 16:38:06 +0300

    fix

--- database/migrations/RoleAndPermissions/2023_08_01_015204_fill_default_roles.php
+++ database/migrations/RoleAndPermissions/2023_08_01_015204_fill_default_roles.php
@@ -5,30 +5,42 @@ use App\Enums\RoleEnum;
 use App\Models\RolesAndPermissions\Permission;
 use App\Models\RolesAndPermissions\Role;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up(): void
     {
-        Permission::create([
-            'name' => PermissionEnum::MANAGE_ALL,
+        $permission = Permission::create([
+            'name' => PermissionEnum::MANAGE_ALL->value,
+            'guard_name' => 'api',
         ]);
         Permission::create([
-            'name' => PermissionEnum::VIEW_DEPARTMENTS,
+            'name' => PermissionEnum::VIEW_DEPARTMENTS->value,
+            'guard_name' => 'api',
         ]);
         Role::create([
-            'name' => RoleEnum::EMPLOYEE_MCPS,
+            'name' => RoleEnum::EMPLOYEE_MCPS->value,
+            'guard_name' => 'api',
         ]);
         Role::create([
-            'name' => RoleEnum::EXTERNAL_EMPLOYEE,
+            'name' => RoleEnum::EXTERNAL_EMPLOYEE->value,
+            'guard_name' => 'api',
         ]);
         $role = Role::create([
-            'name' => RoleEnum::ADMIN,
+            'name' => RoleEnum::ADMIN->value,
+            'guard_name' => 'api',
         ]);
-        $role->givePermissionTo(PermissionEnum::MANAGE_ALL->value);
+        DB::table('role_has_permissions')->insert([
+            'role_id' => $role->id,
+            'permission_id' => $permission->id
+        ]);
     }
 
     public function down(): void
     {
-
+        DB::table('model_has_roles')->whereNotNull('role_id')->delete();
+        DB::table('role_has_permissions')->whereNotNull('role_id')->delete();
+        DB::table('permissions')->whereNotNull('id')->delete();
+        DB::table('roles')->whereNotNull('id')->delete();
     }
 };
